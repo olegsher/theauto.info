163114301a6a55af560b80d95fd5730fdfaa956e
oleg <oleg@ubuntu>
2018-05-29 11:27:38 +0300

    fgthj

---
+++ index.php
@@ -0,0 +1,349 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="Car Rental in Israel">
+<meta name="author" content="">
+<link rel="icon" href="favicon.ico">
+
+<title>Car Rental in Israel</title>
+
+<!-- Bootstrap core CSS -->
+
+<!--     <link rel="stylesheet" href="../css/bootstrap.min.css" > -->
+<!--     <script src="../js/bootstrap.min.js" ></script> -->
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<!-- 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+<!-- Custom styles for this template -->
+<link href="pricing.css" rel="stylesheet">
+
+
+
+<!--     <script src="js/jquery-3.3.1.min.js"></script> -->
+
+</head>
+
+<body>
+<?php include("seo.html"); ?>
+  
+
+<!--     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow"> -->
+<!--       <h5 class="my-0 mr-md-auto font-weight-normal">Company name</h5> -->
+<!--       <nav class="my-2 my-md-0 mr-md-3"> -->
+<!--         <a class="p-2 text-dark" href="#">Features</a> -->
+<!--         <a class="p-2 text-dark" href="#">Enterprise</a> -->
+<!--         <a class="p-2 text-dark" href="#">Support</a> -->
+<!--         <a class="p-2 text-dark" href="#">Pricing</a> -->
+<!--       </nav> -->
+<!--       <a class="btn btn-outline-primary" href="#">Sign up</a> -->
+<!--     </div> -->
+
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h1 class="display-4" style="font-family: Times, serif; color:grey; text-transform: uppercase; color: white; text-shadow: 2px 2px 4px #000000;">Car Rental in Israel<hr /></h1>
+      <p class="lead">No credit card fees | No amendment fees | Unlimited mileage with 3-29 days orders</p>
+      <p class="lead">Contact us by <a style="font-family: Times, serif; color:red">WhatsApp/Viber +972-58-771-0101</a> and our representative will back to you with best offer</p>
+    </div>
+    
+    
+    <div class="container">
+    
+     <p style="color: white; text-shadow: 2px 2px 4px #000000; font-family: Times, serif; text-align: left; font-size: 2em; text-transform: uppercase;">Small Cars</p>
+      <div class="card-deck mb-3 text-center">
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Suzuki Alto or Similar</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$10 <small class="text-muted">/day</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Car Category: B (MBMR)</li>
+              <li>Unlimited mileage with 3-29 days orders</li>
+<!--               <li>Email support</li> -->
+<!--               <li>Help center access</li> -->
+            </ul>
+<!--             <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button> -->
+          </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Fiat 500 or Similar (2 Doors)</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$10 <small class="text-muted">/day</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Car Category: Q (EBAR)</li>
+              <li>Unlimited mileage with 3-29 days orders</li>
+<!--               <li>Priority email support</li> -->
+<!--               <li>Help center access</li> -->
+            </ul>
+<!--             <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button> -->
+          </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Kia Picanto or Similar</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$11 <small class="text-muted">/day</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Car Category: C (EDAR)</li>
+              <li>Unlimited mileage with 3-29 days orders</li>
+<!--               <li>Phone and email support</li> -->
+<!--               <li>Help center access</li> -->
+            </ul>
+<!--             <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button> -->
+          </div>          
+        </div>        
+      </div>
+      
+   <p style="color: white; text-shadow: 2px 2px 4px #000000; font-family: Times, serif; text-align: left; font-size: 2em; text-transform: uppercase;">Premium Cars</p>   
+      
+            <div class="card-deck mb-3 text-center">
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Audi A6 or Similar</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$122 <small class="text-muted">/day</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Car Category: W (LCBR)</li>
+              <li>Unlimited mileage with 3-29 days orders</li>
+<!--               <li>Email support</li> -->
+<!--               <li>Help center access</li> -->
+            </ul>
+<!--             <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button> -->
+          </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Nissan Maxima or Similar</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$79 <small class="text-muted">/day</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Car Category: P (LDAR)</li>
+              <li>Unlimited mileage with 3-29 days orders</li>
+<!--               <li>Priority email support</li> -->
+<!--               <li>Help center access</li> -->
+            </ul>
+<!--             <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button> -->
+          </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">BMW 318i or Similar</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$72 <small class="text-muted">/day</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Car Category: K (UDAR)</li>
+              <li>Unlimited mileage with 3-29 days orders</li>
+<!--               <li>Phone and email support</li> -->
+<!--               <li>Help center access</li> -->
+            </ul>
+<!--             <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button> -->
+          </div>          
+        </div>        
+      </div>
+      
+      
+      <p style="color: white; text-shadow: 2px 2px 4px #000000; font-family: Times, serif; text-align: left; font-size: 2em; text-transform: uppercase;">People carriers</p>
+        
+      
+            <div class="card-deck mb-3 text-center">
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">VW Transporter or Similar (Aut. 9 Seats)</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$95 <small class="text-muted">/day</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Car Category: Z (LVAR)</li>
+              <li>Unlimited mileage with 3-29 days orders</li>
+			  <li><img class="img-responsive" src="https://rent.albar.co.il/CarImages/Rent/CarCategories/260t2.jpg" alt="rent VW Transporter in Israel" /> </li>
+<!--               <li>Help center access</li> -->
+            </ul>
+<!--             <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button> -->
+          </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Kia Carnival or Similar (Aut. 8 Seats)</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$83 <small class="text-muted">/day</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Car Category: V8 (FVAR)</li>
+              <li>Unlimited mileage with 3-29 days orders</li>
+			  <li><img class="img-responsive" src="https://rent.albar.co.il/CarImages/Rent/CarCategories/225.jpg" alt="rent Kia Carnival in Israel" /> </li> 
+<!--               <li>Help center access</li> -->
+            </ul>
+<!--             <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button> -->
+          </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Mitsubishi Outlander or Similar (Aut. 7 Seats)</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$58 <small class="text-muted">/day</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Car Category: V (SVAR)</li>
+              <li>Unlimited mileage with 3-29 days orders</li>
+ 			  <li><img class="img-responsive" src="https://rent.albar.co.il/CarImages/Rent/CarCategories/220t.jpg" alt="rent Mitsubishi Outlander in Israel" /> </li> 
+<!--               <li>Help center access</li> -->
+            </ul>
+<!--             <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button> -->
+          </div>          
+        </div>        
+      </div>
+      
+      
+      
+              
+      
+            <div class="card-deck mb-3 text-center">
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Opel Zafira or Similar (Aut. 7 Seats)</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$39 <small class="text-muted">/day</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Car Category: U (IVAR)</li>
+              <li>Unlimited mileage with 3-29 days orders</li>
+<!--               <li>Email support</li> -->
+<!--               <li>Help center access</li> -->
+            </ul>
+<!--             <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button> -->
+          </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Renault Traffic or Similar (Man. 9 Seats)</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$55 <small class="text-muted">/day</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Car Category: Y (FVMR)</li>
+              <li>Unlimited mileage with 3-29 days orders</li>
+<!--               <li>Priority email support</li> -->
+<li>
+<form action="mail_send.php" method="post">
+  					<div class="form-inline">
+   					 <div class="col-auto">
+    				 <input type="text" class="form-control" id="name" name="name" placeholder="name">
+  					 </div>
+  				  	 <div class="col-auto">
+     				 <input type="tel" class="form-control" id="phone" name="phone" placeholder="phone include country code">
+    			     </div>    			  
+     				 <input type="hidden" name="car-select" value="Y (FVMR), Renault Traffic or Similar (Man. 9 Seats) ">
+  					</div>
+  					<input type="submit" name="submit" value="Send">
+				</form></li> 
+            </ul>
+<!--             <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button> -->
+          </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Ford Edge or Similar</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$74 <small class="text-muted">/day</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Car Category: O (LFBR)</li>
+              <li>Unlimited mileage with 3-29 days orders</li>
+<!--               <li>Phone and email support</li> -->
+               <li>
+               <form action="mail_send.php" method="post">
+  					<div class="form-inline">
+   					 <div class="col-auto">
+    				 <input type="text" class="form-control" id="name" name="name" placeholder="name">
+  					 </div>
+  				  	 <div class="col-auto">
+     				 <input type="tel" class="form-control" id="phone" name="phone" placeholder="phone include country code">
+    			     </div>    			  
+     				 <input type="hidden" name="car-select" value="Ford Edge or Similar O (LFBR)">
+  					</div>
+  					<input type="submit" name="submit" value="Send">
+				</form>
+</li> 
+            </ul>
+<!--             <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button> -->
+          </div>          
+        </div>        
+      </div>
+      
+      
+
+      <footer class="pt-4 my-md-5 pt-md-5 border-top">
+      What do I need to hire a car? <br>
+		To book your car, all you need is a credit card. 
+		<br>When you pick the car up, you’ll need:
+		<br>Your voucher, the main driver’s credit card, with enough available funds for the car’s deposit. Each driver’s full, valid driving license, which they’ve held for at least 12 months (often 24). Your passport.
+
+<!--         <div class="row"> -->
+<!--           <div class="col-12 col-md"> -->
+<!--             <img class="mb-2" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24"> -->
+<!--             <small class="d-block mb-3 text-muted">&copy; 2017-2018</small> -->
+<!--           </div> -->
+<!--           <div class="col-6 col-md"> -->
+<!--             <h5>Features</h5> -->
+<!--             <ul class="list-unstyled text-small"> -->
+<!--               <li><a class="text-muted" href="#">Cool stuff</a></li> -->
+<!--               <li><a class="text-muted" href="#">Random feature</a></li> -->
+<!--               <li><a class="text-muted" href="#">Team feature</a></li> -->
+<!--               <li><a class="text-muted" href="#">Stuff for developers</a></li> -->
+<!--               <li><a class="text-muted" href="#">Another one</a></li> -->
+<!--               <li><a class="text-muted" href="#">Last time</a></li> -->
+<!--             </ul> -->
+<!--           </div> -->
+<!--           <div class="col-6 col-md"> -->
+<!--             <h5>Resources</h5> -->
+<!--             <ul class="list-unstyled text-small"> -->
+<!--               <li><a class="text-muted" href="#">Resource</a></li> -->
+<!--               <li><a class="text-muted" href="#">Resource name</a></li> -->
+<!--               <li><a class="text-muted" href="#">Another resource</a></li> -->
+<!--               <li><a class="text-muted" href="#">Final resource</a></li> -->
+<!--             </ul> -->
+<!--           </div> -->
+<!--           <div class="col-6 col-md"> -->
+<!--             <h5>About</h5> -->
+<!--             <ul class="list-unstyled text-small"> -->
+<!--               <li><a class="text-muted" href="#">Team</a></li> -->
+<!--               <li><a class="text-muted" href="#">Locations</a></li> -->
+<!--               <li><a class="text-muted" href="#">Privacy</a></li> -->
+<!--               <li><a class="text-muted" href="#">Terms</a></li> -->
+<!--             </ul> -->
+<!--           </div> -->
+<!--         </div> -->
+      </footer>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<!--     <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script> -->
+<!--     <script src="../../../../assets/js/vendor/popper.min.js"></script> -->
+<!--     <script src="../../../../dist/js/bootstrap.min.js"></script> -->
+<!--     <script src="../../../../assets/js/vendor/holder.min.js"></script> -->
+    <script>
+      Holder.addTheme('thumb', {
+        bg: '#55595c',
+        fg: '#eceeef',
+        text: 'Thumbnail'
+      });
+    </script>
+  </body>
+</html>
