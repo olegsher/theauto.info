2063081af9a5eace54d8585b85516c0723ef088d
oleg <oleg@ubuntu.Home>
2018-06-03 01:03:57 +0300

    dfghd

---
+++ ru/Europcar_Albar_Vastama_branches.php
@@ -0,0 +1,44 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="ru">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="Аренда прокат автомобиля в Израиле">
+<meta name="author" content="">
+<meta name="yandex-verification" content="f11475564b48ca1f" />
+<meta name="msvalidate.01" content="DFFC9D141B04223AD2603B9AD5606773" />
+<link rel="icon" href="http://www.theauto.info/favicon.ico">
+
+<meta property="fb:app_id" content="1322511981092474" />
+<meta property="og:type" content="website" />
+<meta property="og:locale" content="ru" />
+<meta property="og:url" content="http://www.theauto.info/ru/" />
+<meta property="og:image" content="http://www.theauto.info/img/car_key.jpeg"/>
+<meta property="og:image:type" content="image/png" />
+<meta property="og:image:width" content="640" />
+<meta property="og:image:height" content="426" />
+<meta property="og:title" content="Аренда прокат автомобиля в Израиле" />
+<meta property="og:description" content="Аренда прокат автомобиля в Израиле"/>
+
+
+<title>Аренда прокат автомобиля в Израиле</title>
+
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<!-- 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+<!-- Custom styles for this template -->
+<link href="../pricing.css" rel="stylesheet">
+</head>
+
+<body>
+<?php include("../seo.html"); ?>
+
+<?php include("__branches.html"); ?>
+  </body>
+</html>
