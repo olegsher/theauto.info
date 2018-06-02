e2d51015c62cbfb7939a39f2e9aebd528671d1ba
oleg <oleg@ubuntu.Home>
2018-06-03 01:28:39 +0300

    выаы

--- ru/Europcar_Albar_Vastama_branches.php
+++ ru/Europcar_Albar_Vastama_branches.php
@@ -51,7 +51,7 @@
 
  <div class="container">
  
- <h1 id="standard" style="color: white; text-shadow: 1px 1px 2px #000000; font-family: Times, serif; text-align: left; font-size: 1.5em; text-transform: uppercase;">Отделения проката аренды автомобилей в Израиле</h1>
+<h1 id="standard" style="color: white; text-shadow: 1px 1px 2px #000000; font-family: Times, serif; text-align: left; font-size: 1.5em; text-transform: uppercase;">Отделения проката аренды автомобилей в Израиле</h1>
 <?php include("__branches.html"); ?>
 </div>
 </body>
