2545e05b224419c3c0a7358e4ccab62b95d004a4
oleg <oleg@ubuntu>
2018-06-07 10:18:09 +0300

    google tags

--- index.php
+++ index.php
@@ -35,6 +35,13 @@
 <!-- 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
 <!-- Custom styles for this template -->
 <link href="pricing.css" rel="stylesheet">
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5WSN6CF');</script>
+<!-- End Google Tag Manager -->
 </head>
 
 <body>
