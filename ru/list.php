c6c454198e87c579852f224b48ecfc48711cd0e6
oleg <oleg@ubuntu>
2018-06-25 17:09:35 +0300

    rethyet

--- ru/list.php
+++ ru/list.php
@@ -54,8 +54,9 @@ foreach($a as $key => $value)
 //     echo '<br>' . $value;
 $path = 'news/' . $value;
     
-    echo ' <a rel="nofollow" href="' . $path . ' "title="' . 
-    "Новости мира автомобилей на портале TheAuto.info" . '" >' . $value . '</a> <br />';
+    echo '<a rel="nofollow" href="' . $path . '" title="' . "Новости мира автомобилей на портале TheAuto.info" . '" >' . $value . '</a><br>';
+    echo "\n";
+    // echo '<a rel="nofollow" href="$path" title="Новости мира автомобилей на портале TheAuto.info" > . $value . </a> <br />';
     // rsort($value);
     // print_r(array_reverse($a));
     
