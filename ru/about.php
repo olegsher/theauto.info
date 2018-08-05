8f65d9bf0d368a7669ab346f2efeb29e20525148
olegs <olegs@olegs-VirtualBox>
2018-08-05 12:56:20 +0300

    csv

--- ru/about.php
+++ ru/about.php
@@ -19,6 +19,7 @@
 <h1 id="standard" style="color: white; text-shadow: 1px 1px 2px #000000; font-family: Times, serif; text-align: left; font-size: 1.5em; text-transform: uppercase;">Компания Vastama ltd. Прокат аренда автомобилей в Израиле</h1>
 <?php include("__about.html"); ?>
 
+<?php
 $f = fopen("../pricelist/albar.csv", "r");
 while (($line = fgetcsv($f)) !== false) {
         echo "<tr>";
@@ -28,7 +29,7 @@ while (($line = fgetcsv($f)) !== false) {
         echo "</tr>\n";
 }
 fclose($f);
-
+?>
 
 
 </div>
