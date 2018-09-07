047ef4c0e69702ce12830b8954ea38082eb84404
Oleg Sher <github@sher.biz>
2018-09-07 19:07:35 +0300

    вап

--- index.php
+++ index.php
@@ -52,12 +52,12 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
      <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow"> 
       <h5 class="my-0 mr-md-auto font-weight-normal">Vastama Ltd - your Car Rental agent in Israel</h5> 
-<!--        <nav class="my-2 my-md-0 mr-md-3">  -->
-<!--          <a class="p-2 text-dark" href="#leaders">Leaders</a>  -->
-<!--          <a class="p-2 text-dark" href="#premium">Premium</a>  -->
+       <nav class="my-2 my-md-0 mr-md-3"> 
+         <a class="p-2 text-dark" href="#price_list">Price List</a> 
+         <a class="p-2 text-dark" href="#rent_cond">Rental Conditions</a> 
 <!--          <a class="p-2 text-dark" href="#people_carriers">People Carriers</a>  -->
 <!--          <a class="p-2 text-dark" href="#standard">Standard</a>  -->
-<!--        </nav>  -->
+       </nav> 
        <a class="btn btn-outline-primary" href="/ru/" title="Go to Russian section">Русский</a> 
      </div> 
 
@@ -84,11 +84,11 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             $low_s = array(0);
             
             foreach ($low_s as $c) {
-            echo '<div class="card mb-4 box-shadow"><div class="card-header"><h4 class="my-0 font-weight-normal">'. $data[4].'</h4></div>
+            echo '<div class="card mb-4 box-shadow"><div class="card-header"><h4 id="'. $data[0].'"class="my-0 font-weight-normal">'. $data[4].'</h4></div>
           <div class="card-body">
             <h5 class="card-title pricing-card-title">Starting '. $data[11].'<small class="text-muted"> USD/day</small></h5>
             <ul class="list-unstyled mt-3 mb-4">
-              <li>Car Category: '. $data[0].'  Seats: '. $data[3].'</li>
+              <li>Car Category: '. $data[0].'<br>Seats: '. $data[3].'</li>
               <li>Unlimited mileage with 3-29 days</li>
               <li>Monthly price starting '. $data[13].'USD</li>
               <li><img class="img-fluid" src="img/CarCategories/'. $data[0].'.jpg" alt="rent '. $data[4].' in Israel" /> </li>
@@ -117,12 +117,14 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         fclose($handle);
     }
     ?>
+    Car models are for guidance only and similar car models may be supplied. 
         </div>        
      
 
 
 
     <div class="container">
+    <h2 id="price_list" style="color: white; text-shadow: 1px 1px 2px #000000; font-family: Times, serif; text-align: left; font-size: 1.5em; text-transform: uppercase;">Price List:</h2>
     
     <hr /><strong>Car Rental Israel  - Low Season Price List</strong>($USD): 
     <br>Apr 10 - Jul 14 2018; Aug 26 - Dec 19 2018; Jan 6 - Apr 13 2019; Apr 29 - Jul 14 2019; Aug 26 - Dec 19 2019;
@@ -160,7 +162,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
                 echo '<tr>
                 <td>'. $data[0]. '<br>'. $data[1]. '</td>
-                <td><strong>'. $data[4].'</strong><br>Seats '. $data[3]. '</td>
+                <td><a href="#'. $data[0]. '"><strong>'. $data[4].'</strong></a><br>Seats '. $data[3]. '</td>
                 <td>'. $data[5]. '</td>
                 <td>'. $data[9]. '</td>
                 <td>'. $data[11]. '</td>
@@ -217,7 +219,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
                 echo '<tr>
                 <td>'. $data[0]. '<br>'. $data[1]. '</td>
-                <td><strong>'. $data[4].'</strong><br>Seats '. $data[3]. '</td>
+                <td><a href="#'. $data[0]. '"><strong>'. $data[4].'</strong></a><br>Seats '. $data[3]. '</td>
                 <td>'. $data[6]. '</td>
                 <td>'. $data[10]. '</td>
                 <td>'. $data[12]. '</td>
@@ -245,6 +247,72 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <br>Currency: USD $
 <br>Valid: Dec 19, 2019
 <br>Rates include the BASIC RENTAL ONLY & EXCLUDE Protection Coverage's (Monthly rentals include the Protection Coverage's CDW, TP & 3PLC).
+
+<hr />
+<h2 id="rent_cond" style="color: white; text-shadow: 1px 1px 2px #000000; font-family: Times, serif; text-align: left; font-size: 1.5em; text-transform: uppercase;">Rental Conditions:</h2>
+
+<br>VAT: Israeli passport holders are subject to pay VAT by law including dual citizenship passport holders.
+<br>Only tourists with B2 or B3 visa class permits are waived. 
+<br>Important: The Visa Permit must be presented at the rental desk for VAT to be waived. 
+<br>Airport Fee: One time charge of $39.00 will apply for Pickup or Return of vehicle at the Ben Gurion Airport location. 
+
+<hr />One Way Rentals: FREE within all locations, except for one way To or From the Eilat location as follows: 
+<br>1-2 Day Rentals: $200.00 
+<br>3 Days and over: FREE 
+
+<hr />Loss Damage Waiver & Theft Protection (CDW/LDW & TP) cover damages caused to the vehicle, except the minimum Excess Charges based on car group. 
+<br>An Excess Charge of $500.00-$1,000.00 will apply in case of damage according to vehicle type. CDW/LDW does not cover damage caused to the Tires, Windscreen, Rooftop and underbody damage to the vehicle that will be charged accordingly. 
+<br>Super CDW is optional and will reduce the renter’s responsibility to Zero in case of damage to the vehicle. Can be purchased only in conjunction with CDW/LDW & TP. SCDW does not cover damage caused to the Tires, Windscreen and Rooftop. 
+<br>Super TP is optional and will reduce the renter’s responsibility to Zero in case of theft of the rental vehicle. Can be purchased only in conjunction with CDW/LDW & TP. 
+<br>Liability Coverage (3PLC)- Mandatory for customers that decline CDW/LDW & TP, provided these are covered by the credit card company. Required to cover items not covered by the credit card 
+company as blowout’s, tire/trim damage, underbody damage and vandalism caused not as a result of vehicle collision and third party damage. 
+<br>CDW/LDW & TP can be declined only if renter has sufficient coverage by a US issued World Master Card or Canadian issued Gold/Platinum Visa/Master Card or Visa Premier. 
+<br>Written proof of the credit card company must be provided to confirm valid damage and theft coverage's apply for car rentals in Israel. 
+<br>Maximum security guarantee is up to the full value of the vehicle if CDW/LDW & TP (Damage & Theft coverage's) are declined. 
+<br>Valid for rentals of maximum 28 consecutive days.
+
+<hr />Delivery & Collection is available on request and requires confirmation. 
+<br>Up to 15 KM from Pickup location: $15.00 
+<br>Up to 30 KM from Pickup location: $25.00 
+<br>Up to 50 KM and over from Pickup location: $40.00 
+
+<hr />Car models are for guidance only and similar car models may be supplied. 
+<br>Minimum charge per rental day is 24 hours.
+<br>Late returns of 1 hour and over will be charged at 1/3 of the daily rate. 3 hours and over will be calculated as one extra day.
+<br>Use of vehicle will be at full responsibility of renter as shown on rental agreement. Every additional driver must be registered on the rental agreement as well. Crossing and exiting Israel borders and/or the Palestinian Authority Territories with the rental vehicle is strictly forbidden. 
+<br>Fuel: Supplied with a full tank and recommended to return full. An additional refueling service fee of 30% will apply for vehicles returned with missing fuel. 
+<br>Early return of vehicle prior to the original return date will be charged according to the relevant rental period rate (Daily, Weekly, Monthly, Etc). 
+<br>Parking tickets, Traffic violations & Toll roads: An additional service fee of ILS 50.00 will apply for unpaid tickets ticket violations and toll road fees.  
+
+<hr />Free Sell applies for the following car groups: B(EBMR), Q(EBAR), C(ECAR), D(CCAR), E(CDAR, F(IDAR), I(SDAR), IW(CWAR), H(FDAR), M(PDAR). All other car groups require specific confirmation.
+<br>Cancellations must be sent by 24 hours prior to Pickup Time. Cancellations for 7-8-9 seat vehicles must be sent 48 hours prior to Pickup. 1 day charge will apply for No Shows. 
+
+<hr />Minimum Rental Age & Requirements: A valid Drivers License & Passport must be presented at pickup location. Minimum age of driver is 21 based on car group and requirements as shown below: 
+<br>Groups B(MBMR), Q(EBAR), C(EDAR), D(CBAR), E(CCAR): Minimum age required is 21 (Additional fee applies) + Drivers License valid for at least one year. 
+<br>Groups F(IDAR), FX(DBAR), I(SCAR), IW(CWAR), H(SDAR), M(PDAR), MH(PCAR), J(SFBR), T(CPMR): Minimum age required is 21 (Additional fee applies) + Drivers License valid for at least one year.
+<br>Groups K(UDAR), R(FCAR), O(LFBR), U(IVAR), V(SVAR), V8(FVAR), Y(FVMR), Z(LVAR): Minimum age is 25 + Drivers License valid for at least one year. 
+<br>Groups P(LDAR), W(LCBR): Minimum age required is 28 + Drivers License valid for at least five years. 
+
+<hr />Underage Driver: An additional fee of $12.00 per day (Applies for drivers 21-23 years of age). 
+<br>Additional Driver: $4.00 per day. 
+<br>CSI Child Seat for Infant: $5.00 per day (0-12 months). 
+<br>CSB Child Seat for Baby: $5.00 per day (1-3 years). 
+<br>CST Child Seat for Toddler: $5.00 per day (4-7 years). 
+<br>CBS Child Booster Seat: $5.00 per day (8 years and above).
+<br>Smartphone - Navigation System/GPS: $15.00 per day - Includes Free Smartphone, Unlimited local & international phone calls, Unlimited Data & Wifi Hotspot! 
+<br>Mini iPad - Navigation System/GPS: $18.00 per day - Includes Unlimited Data, VOIP Calls & Wifi Hotspot! 
+<br>Roadsafe Assistance: $4.00 per day (maximum charge $100.00). 
+<br>Express assistance for the following services: Keys locked in the car, lost or broken, Flat tire assistance, Out of fuel or Battery failure? We'll send assistance to get your vehicle started quickly. 
+
+<hr />Payment Methods: Major credit cards are accepted to cover rental costs and additional charges: Visa, MasterCard, Amex, Diners. 
+<br>Credit Card must be on renter's name. 
+
+
+
+
+
+
+
 
 </div> 
 
