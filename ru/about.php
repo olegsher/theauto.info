88968040eca1b96338a2fbfb655b99504d8b260f
Oleg Sher <github@sher.biz>
2018-09-10 22:37:45 +0300

    dfg

--- ru/about.php
+++ ru/about.php
@@ -14,6 +14,7 @@
 <body>
 <?php include("../seo.html"); ?>
 <div class="container">
+<?php include("__carousel.html"); ?>
 <?php include("__menu.html"); ?>
 
 <h1 id="standard" style="color: white; text-shadow: 1px 1px 2px #000000; font-family: Times, serif; text-align: left; font-size: 1.5em; text-transform: uppercase;">Компания Vastama ltd. Прокат аренда автомобилей в Израиле</h1>
