335108b989cb7416b98d81d7a88f20a576e5e64b
olegs <olegs@aaa-VirtualBox>
2018-09-27 15:22:35 +0300

    yuioy

--- ru/list.php
+++ ru/list.php
@@ -6,34 +6,9 @@ echo '<!doctype html>
 <html lang="ru">
 <head>
 <title>Новости мира авто - Прокат аренда авто в Израиле +972-58-7710101</title>
-<meta name="description" content="Новости мира авто - Прокат аренда авто в Израиле. Отделения проката в Бен Гурион, Тель Авив Ашдод Хайфа Эйлат Без предоплаты. Говорим по русски" />
-<meta name="author" content="olegs" />
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-<meta name="yandex-verification" content="f11475564b48ca1f" />
-<meta name=\'wmail-verification\' content=\'a9138f370e313f0907f0d27ff4975904\' />
-<meta name="msvalidate.01" content="DFFC9D141B04223AD2603B9AD5606773" />
-
-<link rel="icon" href="http://www.theauto.info/favicon.ico">
-
-<meta property="fb:app_id" content="1322511981092474" />
-<meta property="og:type" content="website" />
-<meta property="og:locale" content="ru" />
-<meta property="og:url" content="http://www.theauto.info/ru/" />
-<meta property="og:image" content="http://www.theauto.info/img/car_key.jpeg"/>
-<meta property="og:image:type" content="image/png" />
-<meta property="og:image:width" content="640" />
-<meta property="og:image:height" content="426" />
-<meta property="og:title" content="+972-58-7710101 Аренда авто в Израиле. Отделения проката в Бен Гурион, Тель Авив, Эйлат, Иерусалим и др. Без предоплаты. Говорим по русски" />
-<meta property="og:description" content="+972-58-7710101 Аренда авто в Израиле. Лучшие цены на аренду авто в Израиле. Отделения проката в Бен Гурион, Тель Авив, Эйлат, Иерусалим и др. Без предоплаты. Говорим по русски"/>
-<!-- Bootstrap core CSS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<link rel="stylesheet" href=https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css>
-<!-- Custom styles for this template -->
-<link href="../pricing.css" rel="stylesheet">
+<meta name="description" content="Новости мира авто - Прокат аренда авто в Израиле. Отделения проката в Бен Гурион, Тель Авив Ашдод Хайфа Эйлат Без предоплаты. Говорим по русски" /> ';
+include("__header.html");
+echo '
 </head>
 <body>
 <div class="container">
@@ -54,7 +29,7 @@ foreach($a as $key => $value)
 //     echo '<br>' . $value;
 $path = 'news/' . $value;
 $newname = preg_replace('/-/', ' ', $value);
-$newname = preg_replace('/.html/', ' ', $newname);
+$newname = preg_replace('/.php/', ' ', $newname);
 
     
     echo '<a rel="nofollow" href="' . $path . '" title="' . "Новости мира автомобилей на портале TheAuto.info" . '" >' . $newname . '</a><br>';
