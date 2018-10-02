4c6e4b19a1db4f2cf732d851d8cfc272bb1dbf0d
olegs <olegs@aaa-VirtualBox>
2018-10-02 14:40:41 +0300

    fgdf

--- ru/qa.php
+++ ru/qa.php
@@ -13,29 +13,9 @@
 
 
 
-<nav class="navbar navbar-color-on-scroll navbar-transparent fixed-top navbar-expand-lg"  color-on-scroll="100">
+<nav class="navbar navbar-light bg-primary">
   <div class="container">
-      <div class="navbar-translate">
-        <a class="navbar-brand" href="http://www.theauto.info/ru">
-          Material Kit </a>
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="navbar-toggler-icon"></span>
-            <span class="navbar-toggler-icon"></span>
-            <span class="navbar-toggler-icon"></span>
-        </button>
-      </div>
-
-      <div class="collapse navbar-collapse">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a href="#" class="nav-link">
-                  <i class="material-icons">apps</i> Template
-              </a>
-            </li>
-          </ul>
-      </div>
+    <a class="navbar-brand" href="#">Default</a>
   </div>
 </nav>
 
