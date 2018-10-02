94b6e2812d7d8ee20b7ac3fc0271e9d9ba5e3fa4
olegs <olegs@aaa-VirtualBox>
2018-10-02 14:43:36 +0300

    fgthjutghy

--- ru/qa.php
+++ ru/qa.php
@@ -13,9 +13,36 @@
 
 
 
-<nav class="navbar navbar-light bg-primary">
+<nav class="navbar navbar-expand-lg bg-primary">
   <div class="container">
-    <a class="navbar-brand" href="#">Default</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-bar navbar-kebab"></span>
+    <span class="navbar-toggler-bar navbar-kebab"></span>
+    <span class="navbar-toggler-bar navbar-kebab"></span>
+    </button>
+    <a class="navbar-brand" href="#">Navbar</a>
+
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+      <form class="form-inline ml-auto">
+          <div class="form-group no-border">
+            <input type="text" class="form-control" placeholder="Search">
+          </div>
+          <button type="submit" class="btn btn-white btn-just-icon btn-round">
+              <i class="material-icons">search</i>
+          </button>
+      </form>
+    </div>
   </div>
 </nav>
 
