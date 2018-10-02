d5e95fe038a7b9ba94151c533ed42f31eb0a2fca
olegs <olegs@aaa-VirtualBox>
2018-10-02 15:58:46 +0300

    ghj

--- ru/qa.php
+++ ru/qa.php
@@ -12,7 +12,7 @@
 <body>
 
 
-<div class="navbar navbar-top navbar-expand-md mb-4" role="navigation">
+<div class="navbar fixed-top navbar-expand-md mb-4" role="navigation">
 <div class="container">
     <a class="navbar-brand" href="#">Vastama - Аренда авто в Израиле</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
