38e238478a503f584eb496487268c10c37b833c1
olegs <olegs@aaa-VirtualBox>
2018-10-02 12:22:01 +0300

    dfhgf

--- ru/qa.php
+++ ru/qa.php
@@ -7,7 +7,7 @@
 <meta name="description" content="Вопросы и ответы по аренде автомобиля в Израиле">
 <meta name="author" content="Vastama ltd" >
 
-<?php include("__header.html"); ?>
+<?php include("../_header_include.html"); ?>
 
 
 </head>
@@ -74,6 +74,6 @@
 <?php include("__qa.html"); ?>
 </div>
 
-
+<?php include("../_bottom_include.html"); ?>
 </body>
 </html>
