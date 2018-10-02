683956199767d11a146e6229cbec43e7cca84e13
olegs <olegs@aaa-VirtualBox>
2018-10-02 12:33:16 +0300

    fgthfrtg

--- ru/qa.php
+++ ru/qa.php
@@ -3,13 +3,10 @@
 <!doctype html>
 <html lang="ru">
 <head>
+<?php include("../_header_include.html"); ?>
 <title>Вопросы и ответы по аренде автомобиля в Израиле</title>
 <meta name="description" content="Вопросы и ответы по аренде автомобиля в Израиле">
 <meta name="author" content="Vastama ltd" >
-
-<?php include("../_header_include.html"); ?>
-
-
 </head>
 
 <body>
