d0448707718e7395a5f4305ab6fec0c1081d964a
olegs <olegs@aaa-VirtualBox>
2018-10-02 15:38:29 +0300

    апро

--- ru/qa.php
+++ ru/qa.php
@@ -22,60 +22,11 @@
             <li class="nav-item active">
                 <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
-<!--             <li class="nav-item"> -->
-<!--                 <a class="nav-link" href="http://fontenele.github.io/bootstrap-navbar-dropdowns/" target="_blank">Github</a> -->
-<!--             </li> -->
-<!--             <li class="nav-item"> -->
-<!--                 <a class="nav-link disabled" href="#">Disabled</a> -->
-<!--             </li> -->
-<!--             <li class="nav-item dropdown"> -->
-<!--                 <a class="nav-link dropdown-toggle" id="dropdown1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown1</a> -->
-<!--                 <ul class="dropdown-menu" aria-labelledby="dropdown1"> -->
-<!--                     <li class="dropdown-item" href="http://www.theauto.info/ru/"><a>Автомобили</a></li> -->
-<!--                     <li class="dropdown-item dropdown"> -->
-<!--                         <a class="dropdown-toggle" id="dropdown1-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown1.1</a> -->
-<!--                         <ul class="dropdown-menu" aria-labelledby="dropdown1-1"> -->
-<!--                             <li class="dropdown-item" href="#"><a>Action 1.1</a></li> -->
-<!--                             <li class="dropdown-item dropdown"> -->
-<!--                                 <a class="dropdown-toggle" id="dropdown1-1-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown1.1.1</a> -->
-<!--                                 <ul class="dropdown-menu" aria-labelledby="dropdown1-1-1"> -->
-<!--                                     <li class="dropdown-item" href="#"><a>Action 1.1.1</a></li> -->
-<!--                                 </ul> -->
-<!--                             </li> -->
-<!--                         </ul> -->
-<!--                     </li> -->
-<!--                 </ul> -->
-<!--             </li> -->
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="dropdown2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown2</a>
+                <a class="nav-link dropdown-toggle" id="dropdown2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Автомобили</a>
                 <ul class="dropdown-menu" aria-labelledby="dropdown2">
-                    <li class="dropdown-item" href="http://www.theauto.info/ru/lux.php"><a>Action 2 A</a></li>
-                    <li class="dropdown-item" href="http://www.theauto.info/ru/minivan.php"><a>Action 2 B</a></li>
-<!--                     <li class="dropdown-item" href="#"><a>Action 2 C</a></li> -->
-                    <li class="dropdown-item dropdown">
-                        <a class="dropdown-toggle" id="dropdown2-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown2.1</a>
-                        <ul class="dropdown-menu" aria-labelledby="dropdown2-1">
-                            <li class="dropdown-item" href="#"><a>Action 2.1 A</a></li>
-                            <li class="dropdown-item" href="#"><a>Action 2.1 B</a></li>
-                            <li class="dropdown-item" href="#"><a>Action 2.1 C</a></li>
-                            <li class="dropdown-item dropdown">
-                                <a class="dropdown-toggle" id="dropdown2-1-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown2.1.1</a>
-                                <ul class="dropdown-menu" aria-labelledby="dropdown2-1-1">
-                                    <li class="dropdown-item" href="#"><a>Action 2.1.1 A</a></li>
-                                    <li class="dropdown-item" href="#"><a>Action 2.1.1 B</a></li>
-                                    <li class="dropdown-item" href="#"><a>Action 2.1.1 C</a></li>
-                                </ul>
-                            </li>
-                            <li class="dropdown-item dropdown">
-                                <a class="dropdown-toggle" id="dropdown2-1-2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown2.1.2</a>
-                                <ul class="dropdown-menu" aria-labelledby="dropdown2-1-2">
-                                    <li class="dropdown-item" href="#"><a>Action 2.1.2 A</a></li>
-                                    <li class="dropdown-item" href="#"><a>Action 2.1.2 B</a></li>
-                                    <li class="dropdown-item" href="#"><a>Action 2.1.2 C</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
+                    <li class="dropdown-item" href="http://www.theauto.info/ru/lux.php"><a>Люкс</a></li>
+                    <li class="dropdown-item" href="http://www.theauto.info/ru/minivan.php"><a>Минивэны</a></li>                   
                 </ul>
             </li>
         </ul>        
