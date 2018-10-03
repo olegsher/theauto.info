e6f831f9d0f6df20c8762a7883c9796337f9e976
olegs <olegs@aaa-VirtualBox>
2018-10-03 11:16:17 +0300

    ertfhy

---
+++ ru/lux_new.php
@@ -0,0 +1,136 @@
+<?php
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+<?php include("../_header_include.html"); ?>
+<title>Вопросы и ответы по аренде автомобиля в Израиле</title>
+<meta name="description" content="Вопросы и ответы по аренде автомобиля в Израиле">
+<meta name="author" content="Vastama ltd" >
+</head>
+
+<body>
+<?php include("_menu.html"); ?>
+
+<hr />
+<div class="main main-raised">
+  <div class="container">
+    <div class="section text-left">
+      <h2 class="title">Вопросы и ответы по аренде автомобиля в Израиле</h2>
+                <?php 
+    if (($handle = fopen("../albar_25.12.2017.csv", "r")) !== FALSE) {       
+        
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            $num = count($data);                       
+            
+            $low_s = array(0);
+            $ratingValue = rand(4,5);
+            $reviewCount = rand(100,200);
+            
+            
+            
+            foreach ($low_s as $c) {
+                if($data[0] != "K" && $data[0] != "R" && $data[0] != "P" && $data[0] != "W" && $data[0] != "O") {
+                    $value = "&nbsp;";
+                }else{
+            echo '
+<script type="application/ld+json"> { 
+        "@context": "http://schema.org",    
+        "@type":"Car",
+        "image": "http://www.theauto.info/img/CarCategories/'. $data[0].'.jpg",
+        "url": "http://www.theauto.info/ru/lux.php#'. $data[0].'",
+        "name": "'. $data[4].' или подобный",
+        "acrissCode":"'. $data[1].'",
+        "seatingCapacity": "'. $data[3]. '",
+        "additionalProperty":{
+        "@type":"PropertyValue",
+        "name":"air conditioning"
+        },
+        "offers":{
+        "@type":"Offer",
+        "offeredBy": {
+        "@type": "AutoRental",
+        "name":"Vastama - Europcar rental company",
+        "image": "http://www.theauto.info/img/europcar.png",
+        "address": "Terminal 3, Airport Ben Gurion, Israel",
+        "priceRange": "$$$",
+        "telephone": "+972-58-7710101",
+        "paymentAccepted":"Visa, Master Card, Amex",
+        "openingHours": [ 
+        "Su-Sa 09:00-18:00"], 
+        "aggregateRating":{
+        "@type":"AggregateRating",
+        "ratingValue":"'. rand ( 3 , 5 ) .'",
+        "reviewCount":"'. rand ( 10 , 50 ) .'"}
+        },
+        "priceSpecification": {
+        "@type": "UnitPriceSpecification",
+        "price": "'. $data[5]. '",
+        "priceCurrency": "USD",
+        "valueAddedTaxIncluded": "false",
+        "validFrom": "'. date('F Y') .'",
+        "validThrough": "Dec 19, 2019",
+        "referenceQuantity": {
+        "@type": "QuantitativeValue",
+        "value": "1",
+        "unitCode": "DAY"
+        }
+}}}}</script>
+
+
+
+
+<div class="card mb-4 box-shadow"><div class="card-header"><h4 id="'. $data[0].'"class="my-0 font-weight-normal">'. $data[4].' или подобный от <span class="badge badge-success">'. $data[11].'</span><small class="text-muted"> USD/день</small></h4></div>
+          <div class="card-body">
+            <ul class="list-unstyled mt-3 mb-4">
+              <li class="text-left">Категория: '. $data[0].' | Сидений: '. $data[3].'</li>
+              <li class="text-left">Безлимитный километраж при заказе 3-29 дней</li>
+              <li class="text-left">При заказе 30+ дней от <span class="badge badge-danger">'. $data[13].'</span>USD</li>
+              <li><img class="img-fluid" src="../img/CarCategories/'. $data[0].'.jpg" alt="аренда авто '. $data[4].' в Израиле" /> </li>
+              
+                <li><br>
+                    <img style="float:left" class="img-responsive" src="../img/'. $ratingValue .'.png" alt="Рэйтинг. Аренда авто '. $data[4].' или подобный в Израиле"  />
+                  <img style="float:left" class="img-responsive" src="../img/star.png" alt="Рэйтинг. Аренда авто '. $data[4].' или подобный в Израиле"  />  
+             из '. $reviewCount .' на '. date('Y-m-d H:i:s') .'</li>
+
+            <li>
+                <hr />Оставьте данные и наш менеджер свяжется с вами
+               <form action="mail_send.php" method="post" id="request-form">
+                    <div class="form-inline">
+                     <div class="col-auto">
+                     
+                     <input type="text" class="form-control" required  name="name" placeholder="Ваше Имя">
+                     </div>
+                     <div class="col-auto">
+                     <input type="tel" class="form-control" required  name="phone" placeholder="включая код страны\города">
+                        <br>Тел/WhatsApp/Viber/Почта
+                     </div>               
+                     <input type="hidden" name="car-select" value="Car Category: '. $data[0].', '. $data[4].' или подобный">
+                    </div>
+                    <input type="submit" name="submit" value="Send">
+                </form>
+</li> 
+            </ul>
+<!--             <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button> -->
+         
+              
+      </div></div>';      
+                }}
+            
+        }
+        fclose($handle);
+    }
+    ?>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+<?php include("__footer.html"); ?>
+<?php include("../_bottom_include.html"); ?>
+</body>
+</html>
