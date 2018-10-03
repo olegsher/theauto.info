e3b0f8ad5cb830fae1de2a7588b99893085920e2
olegs <olegs@aaa-VirtualBox>
2018-10-03 15:13:53 +0300

    sdfghdf

--- ru/tours/index.php
+++ ru/tours/index.php
@@ -29,6 +29,14 @@ echo '
     <p class="card-text">Last updated 3 mins ago</p>
   </div>
 </div>
+<div class="card bg-dark text-white">
+  <img class="card-img" src="https://atlantistravel.co.il/images/excursion/thumb_icon279_5b4351a93acea.jpg" alt="Card image">
+  <div class="card-img-overlay">
+    <h4 class="card-title">Card title</h4>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text">Last updated 3 mins ago</p>
+  </div>
+</div>
        
     </div>
   </div>
