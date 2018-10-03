d293c370a0bbaf4a65cb860fdd2d6a462bb1015b
olegs <olegs@aaa-VirtualBox>
2018-10-03 15:42:35 +0300

    dfg

--- ru/tours/index.php
+++ ru/tours/index.php
@@ -21,6 +21,7 @@ echo '
     <div class="section text-left">
       <h2 class="title">'. $title .'</h2>
 <div class="card-columns text-center">
+<div class="card mb-4 box-shadow">
 <div class="card bg-dark text-white">
   <img class="card-img" src="https://atlantistravel.co.il/images/excursion/thumb_icon279_5b4351a93acea.jpg" alt="Card image">
   <div class="card-img-overlay">
@@ -28,6 +29,7 @@ echo '
     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
     <p class="card-text">Last updated 3 mins ago</p>
   </div>
+</div>
 </div>
 <div class="card bg-dark text-white">
   <img class="card-img" src="https://atlantistravel.co.il/images/excursion/thumb_icon279_5b4351a93acea.jpg" alt="Card image">
