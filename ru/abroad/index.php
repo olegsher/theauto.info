ccb32e448fb19ebb4021497453f8bb8e3ea4ab31
olegs <olegs@aaa-VirtualBox>
2018-10-03 14:03:09 +0300

    abroad

---
+++ ru/abroad/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="ru">
+<head>
+<?php include("../../_header_include.html"); ?>
+<title>Аренда автомобиля по всему миру</title>
+<meta name="description" content="Аренда автомобиля по всему миру">
+<meta name="author" content="Vastama ltd" >
+</head>
+
+<body>
+<?php include("../_menu.html"); ?>
+
+<hr />
+<div class="main main-raised">
+  <div class="container">
+    <div class="section text-left">
+      <h2 class="title">Аренда автомобиля по всему миру</h2>
+      <?php include(""); ?>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+<?php include("../__footer.html"); ?>
+<?php include("../../_bottom_include.html"); ?>
+</body>
+</html>
