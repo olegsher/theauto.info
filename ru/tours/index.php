7853600a1d744416ebcb3b56196c77aecffc8aa2
olegs <olegs@aaa-VirtualBox>
2018-10-03 15:53:37 +0300

    dfgd

--- ru/tours/index.php
+++ ru/tours/index.php
@@ -71,6 +71,32 @@ echo '
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
+
+<div class="card">
+    <img class="card-img-top" src="https://atlantistravel.co.il/images/excursion/thumb_icon279_5b4351a93acea.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">I a broader card with text below as a natural lead-in to extra content. This content is a little longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="https://atlantistravel.co.il/images/excursion/thumb_icon290_5ac33bc50c5a5.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="https://atlantistravel.co.il/images/excursion/thumb_icon424_5b8d4ca8d4cf1.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text"> a broader card with text below as a natural lead-in to extra content. This content is a little longer.This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+
 </div>
 
      
