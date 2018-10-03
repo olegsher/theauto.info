313446b01ddec9e02496b181e980a0b0983a633d
olegs <olegs@aaa-VirtualBox>
2018-10-03 11:52:37 +0300

    bhkmhj

--- ru/lux_new.php
+++ ru/lux_new.php
@@ -103,8 +103,7 @@
           <div class="card-body">
             <ul class="list-unstyled mt-3 mb-4">
               <li class="text-left">Категория: '. $data[0].' | Сидений: '. $data[3].'</li>
-              <li class="text-left">Безлимитный километраж при заказе 3-29 дней</li>
-              <li class="text-left">При заказе 30+ дней от <span class="badge badge-danger">'. $data[13].'</span>USD</li>
+              <li class="text-left">Безлимитный километраж при заказе 3-29 дней</li>              
               <li class="text-left"><img style="float:left" class="img-responsive" src="../img/'. $sale .'.png" alt="" width="64"  /> При заказе 30+ дней от <span class="badge badge-danger">'. $data[13].'</span>USD в месяц(в цену включены обязательные страховки и 150км\день)</li>
               <li><img class="img-fluid" src="../img/CarCategories/'. $data[0].'.jpg" alt="аренда авто '. $data[4].' в Израиле" /> </li>
               
