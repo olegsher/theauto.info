2a3267a0941fa437e6687123fb9bb5a7e0a390fe
olegs <olegs@aaa-VirtualBox>
2018-10-04 10:54:52 +0300

    dgh

---
+++ start.php
@@ -0,0 +1,26 @@
+<?php
+$title = "Start page"
+?>
+<!doctype html>
+<html lang="en">
+<head>
+<?php include("_header_include.html"); ?>
+<title>'. $title .'</title>
+<meta name="description" content="'. $title .'">
+<meta name="author" content="Vastama ltd" >
+</head>
+
+<body>
+<div class="main main-raised">
+  <div class="container">
+    <div class="section text-left">
+      <h2 class="title">'. $title .'</h2>
+      
+    </div>
+  </div>
+</div>
+
+<?php include("__footer.html"); ?>
+<?php include("_bottom_include.html"); ?>
+</body>
+</html>
