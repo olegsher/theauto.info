52392767d0c9d7bea831bf13dd351daa76516b82
olegs <olegs@aaa-VirtualBox>
2018-10-04 11:40:17 +0300

    dfgdf

--- start.php
+++ start.php
@@ -13,7 +13,7 @@
 <body>
 <div class="main main-raised">
   <div class="container">
-    <div class="section text-left">
+    <div class="d-flex justify-content-center align-items-center" style="height:100px;">
       <h2 class="title">        
        <a href="http://www.theauto.info/en" class="btn btn-info btn-round btn-lg " role="button" aria-disabled="false">English</a>       		
        <a href="http://www.theauto.info/ru" class="btn btn-info btn-round btn-lg " role="button" aria-disabled="false">Русский</a>  
@@ -22,11 +22,6 @@
   </div>
 </div>
 
-<div class="h-100 row align-items-center">
-  			<div class="col" style="background:red">
-    			TEXT
-  			</div>
-		</div>
 
 <?php include("__footer.html"); ?>
 <?php include("_bottom_include.html"); ?>
