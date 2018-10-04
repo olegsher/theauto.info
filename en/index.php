879ac47db7f837ad43adb7d80cdf611dd48a93f8
olegs <olegs@aaa-VirtualBox>
2018-10-04 12:00:37 +0300

    ghjkh

--- en/index.php
+++ en/index.php
@@ -25,24 +25,6 @@ $title = "Start page";
 
 
 <title>Car Rental in Israel</title>
-<!--  <link href="css/styles-red.css" rel="stylesheet"> -->
-<!-- Bootstrap core CSS -->
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<!-- Popper JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-
-<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous"> -->
-<!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script> -->
-<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script> -->
-<!-- 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
-<!-- Custom styles for this template -->
-<link href="pricing.css" rel="stylesheet">
 
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -58,7 +40,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 </head>
 
 <body>
-<?php include("seo.html"); ?>
+<?php include("../seo.html"); ?>
 <div class="container">
   
 
@@ -90,7 +72,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         
           
           <?php 
-    if (($handle = fopen("albar_25.12.2017.csv", "r")) !== FALSE) {       
+    if (($handle = fopen("../albar_25.12.2017.csv", "r")) !== FALSE) {       
         
         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
 //             $num = count($data);                       
@@ -219,7 +201,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     </thead>
     <tbody>
     <?php 
-    if (($handle = fopen("albar_25.12.2017.csv", "r")) !== FALSE) {       
+    if (($handle = fopen("../albar_25.12.2017.csv", "r")) !== FALSE) {       
         
         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
             $num = count($data);                       
@@ -276,7 +258,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     </thead>
     <tbody>
     <?php 
-    if (($handle = fopen("albar_25.12.2017.csv", "r")) !== FALSE) {       
+    if (($handle = fopen("../albar_25.12.2017.csv", "r")) !== FALSE) {       
         
         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
             $num = count($data);                       
@@ -389,21 +371,7 @@ company as blowout’s, tire/trim damage, underbody damage and vandalism caused 
 
 </div> 
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<!--     <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script> -->
-<!--     <script src="../../../../assets/js/vendor/popper.min.js"></script> -->
-<!--     <script src="../../../../dist/js/bootstrap.min.js"></script> -->
-<!--     <script src="../../../../assets/js/vendor/holder.min.js"></script> -->
-    <script>
-      Holder.addTheme('thumb', {
-        bg: '#55595c',
-        fg: '#eceeef',
-        text: 'Thumbnail'
-      });
-    </script>
+    
     
 <?php include("../__footer.html"); ?>
 <?php include("../_bottom_include.html"); ?>
