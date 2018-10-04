56cbdb90e9004ea73dc3e9fc7a0e48500779774a
olegs <olegs@aaa-VirtualBox>
2018-10-04 11:52:44 +0300

    ljkhlk

--- start.php
+++ start.php
@@ -11,15 +11,19 @@
 </head>
 
 <body>
-<div class="main main-raised">
-<!--   <div class="container"> -->
     <div class="d-flex justify-content-center align-items-center" style="height:100px;">
       <h2 class="title">        
        <a href="http://www.theauto.info/en" class="btn btn-info btn-round btn-lg " role="button" aria-disabled="false">English</a>       		
        <a href="http://www.theauto.info/ru" class="btn btn-info btn-round btn-lg " role="button" aria-disabled="false">Русский</a>  
        </h2> 
     </div>
-<!--   </div> -->
+
+
+
+<div class="main main-raised">
+ <div class="container"> 
+    hjgfghjg
+ </div> 
 </div> 
 
 
