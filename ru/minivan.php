4ef3f8b881f85f6f9b53d04a163d57e410bc9ac7
olegs <olegs@aaa-VirtualBox>
2018-11-13 11:57:26 +0200

    fsghfgh

--- ru/minivan.php
+++ ru/minivan.php
@@ -139,9 +139,7 @@
                      </div>               
                      <input type="hidden" name="car-select" value="Car Category: '. $data[0].', '. $data[4].' или подобный">
                     </div>
-                    <div class="col-auto"> 
-                    <input type="number"  required name="captcha"  value ="' . date(Y) .'" placeholder="введите год">
-                     </div>
+                    
                      
                     <div class="col-auto">
                     <input type="submit" name="submit" value="Отправить">
